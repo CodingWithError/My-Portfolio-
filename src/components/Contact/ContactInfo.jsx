cf75d7e5ea80b15aab88a01810f769b29dab451f
V Shravanth <143279056+CodingWithError@users.noreply.github.com>
2024-07-26 01:28:35 +0530

    Update ContactInfo.jsx

--- src/components/Contact/ContactInfo.jsx
+++ src/components/Contact/ContactInfo.jsx
@@ -9,7 +9,7 @@ const ContactInfo = () => {
       <div className={styles.socialLinks}>
         <a href="https://www.linkedin.com/in/v-shravanth-43b469274?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         <a href="https://www.instagram.com/nanunihal?igsh=Ym1qb2p3ZDI4NnFp&utm_source=qr" target="_blank" rel="noopener noreferrer">Instagram</a>
-        <a href="https://github.com/sailingsam/" target="_blank" rel="noopener noreferrer">Github</a>
+        <a href="https://github.com/CodingWithError" target="_blank" rel="noopener noreferrer">Github</a>
       </div>
     </div>
   );
